54670c6f6545a6e21713d0ca53edc99e854dad76
yangsheng <yangsheng@youxin.com>
2020-07-23 11:19:20 +0800

    feat: nullable

---
+++ Types/src/main/kotlin/com/jdqm/kotlin/types/nullable/Person.java
@@ -0,0 +1,12 @@
+package com.jdqm.kotlin.types.nullable;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Person {
+
+    @NotNull
+    public String getTitle(){
+        return "null";
+    }
+
+}
